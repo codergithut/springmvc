4c5b7d64ccf2eccf7495b0923b2f8a900ccd5f64
tianjian <1731857742@qq.com>
2017-03-23 16:20:58 +0800

    add coder

--- lucene/src/main/java/searching/PhraseQueryTest.java
+++ lucene/src/main/java/searching/PhraseQueryTest.java
@@ -6,9 +6,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.PhraseQuery;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
@@ -74,6 +72,37 @@ public class PhraseQueryTest extends TestCase {
 
         assertTrue("bingo", matched(new String[] {"lazy", "jumped", "quick"}, 8));
     }
+
+    private void indexSingleFieldDocs(Field[] fields) throws IOException {
+        IndexWriter writer = new IndexWriter(dir, new WhitespaceAnalyzer(),
+                IndexWriter.MaxFieldLength.UNLIMITED);
+        for(Field f : fields){
+            Document doc = new Document();
+            doc.add(f);
+            writer.addDocument(doc);
+        }
+        writer.optimize();
+        writer.close();
+    }
+
+    public void testWildcard() throws IOException {
+        indexSingleFieldDocs(new Field[]
+                {
+                        new Field("contents", "wild", Field.Store.YES, Field.Index.ANALYZED ),
+                        new Field("contents", "child", Field.Store.YES, Field.Index.ANALYZED),
+                        new Field("contents", "mild", Field.Store.YES, Field.Index.ANALYZED),
+                        new Field("contents", "mildew", Field.Store.YES, Field.Index.ANALYZED)
+                });
+
+        IndexSearcher searcher = new IndexSearcher(dir);
+        Query query = new WildcardQuery(new Term("contents", "?ild*"));
+        TopDocs matches = searcher.search(query, 10);
+        assertEquals("child no match", 3, matches.totalHits);
+        assertEquals("score the same", matches.scoreDocs[0].score, matches.scoreDocs[1].score, 0.0);
+        assertEquals("score the same", matches.scoreDocs[1].score, matches.scoreDocs[2].score, 0.0);
+        searcher.close();
+    }
+
 
 
 }
